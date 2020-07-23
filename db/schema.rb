8a9e21128338b8602a5d6c3841607c007a6fdf0c
jarogers095 <jarogers095@gmail.com>
2020-07-22 20:27:56 -0700

    Modified link to registration page from home page, migrated database, created empty views for each model's actions.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_07_23_020750) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "subject"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "zipcode"
+    t.integer "rent"
+    t.integer "rooms_in_house"
+    t.integer "size"
+    t.boolean "private_bathroom"
+    t.boolean "private_entrance"
+    t.boolean "cats"
+    t.boolean "dogs"
+    t.integer "pet_rent"
+    t.integer "tenants"
+    t.string "gender_preference"
+    t.string "image1"
+    t.string "image2"
+    t.string "image3"
+    t.string "image4"
+    t.string "image5"
+    t.string "image6"
+    t.string "image7"
+    t.string "image8"
+    t.string "image9"
+    t.string "image10"
+    t.string "image11"
+    t.string "image12"
+    t.string "image13"
+    t.string "image14"
+    t.string "image15"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
