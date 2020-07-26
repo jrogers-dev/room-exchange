ef25aea81502eba4b8bc8aa3aa6251f1d38425cc
jarogers095 <jarogers095@gmail.com>
2020-07-25 20:10:21 -0700

    Adjusted code in views/owners/new to :password instead of :password_digest, fleshed out OwnersController with create action and a private method for permitting params

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,20 @@
 class OwnersController < ApplicationController
+    def new
+
+    end
+    
+    def create 
+        @owner = Owner.new(owner_params)
+        if @owner.save
+            redirect_to "/login"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def owner_params
+        params.require(:owner).permit(:email, :password)
+    end
 end
